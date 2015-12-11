578dcb136a90b90850824388bdf0917fafdf2816
Julian Meanchoff <jpmeanchoff@gmail.com>
2015-12-11 14:48:03 -0500

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp   = require('gulp'),
+		sass = require('gulp-sass'),
+		autoprefixer = require('gulp-autoprefixer'),
+		concat = require('gulp-concat'),
+		browserSync = require('browser-sync'),
+		reload = browserSync.reload;
+
+gulp.task('bs', function(){
+	browserSync.init({
+		proxy:'http://localhost:8888/julianmeanchoff-week7project/'
+	});
+});
+
+gulp.task('default', ['bs','styles', 'watch']);
+
+gulp.task('styles', function() {
+	return gulp.src('sass/**/*.scss')
+		.pipe(sass({
+			'sourcemap=none': true,
+			errLogToConsole: true
+		}))
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest('.'))
+		.pipe(reload({ stream:true }));
+});
+
+// configure which files to watch and what tasks to use on file changes
+gulp.task('watch', function() {
+	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('**/*.php', reload);
+});
