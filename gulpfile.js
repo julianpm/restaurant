cc0fecd2e53ce50bb0d3bd5c15eff7db11b5a1c9
Julian Meanchoff <jpmeanchoff@gmail.com>
2015-12-17 20:40:53 -0500

    added more to second section

--- gulpfile.js
+++ gulpfile.js
@@ -7,25 +7,28 @@ var gulp   = require('gulp'),
 
 gulp.task('bs', function(){
 	browserSync.init({
-		proxy:'http://localhost:8888/julianmeanchoff-week7project/'
+		// proxy:'http://localhost:3000/restaurantpsd'
+		server:{
+			baseDir: './'
+		}
 	});
 });
 
 gulp.task('default', ['bs','styles', 'watch']);
 
 gulp.task('styles', function() {
-	return gulp.src('sass/**/*.scss')
+	return gulp.src('styles/**/*.scss')
 		.pipe(sass({
 			'sourcemap=none': true,
 			errLogToConsole: true
 		}))
 		.pipe(concat('style.css'))
-		.pipe(gulp.dest('.'))
+		.pipe(gulp.dest('styles/'))
 		.pipe(reload({ stream:true }));
 });
 
 // configure which files to watch and what tasks to use on file changes
 gulp.task('watch', function() {
-	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('styles/**/*.scss', ['styles']);
 	gulp.watch('**/*.php', reload);
 });
