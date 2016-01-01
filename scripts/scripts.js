f5e3317daf7b679ee4e6dc4472a9dc204e731eae
Julian Meanchoff <jpmeanchoff@gmail.com>
2015-12-31 21:47:51 -0500

    worked on hover states of Our Team section

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,15 +3,6 @@
 $(document).ready(function(){
 	$('.slider').slick({
 		dots:true,
-		arrows:false,
 		speed:300
 	});
 });
-
-// $(document).ready(function(){
-// 	$('.portfolioSlider').slick({
-// 		dots:true,
-// 		arrows:false,
-// 		speed:300
-// 	});
-// });
